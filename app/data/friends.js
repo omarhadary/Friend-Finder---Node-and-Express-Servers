117c7e571655a13ec4f58bfb3de7c17cbede0f6c
Omar Elhadary <Omar Elhadary>
2017-01-12 00:24:41 -0800

    posting firends object to api/friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,8 +3,23 @@ var friendsArray = [
     {
         name: "Ahmed",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [5, 1, 4, 5, 3, 2, 1, 3, 2, 3]
-    }
+        scores: ["5", "1", "4", "5", "3", "2", "1", "3", "2", "3"]
+    },
+    {
+        name: "Lebowski",
+        photo: "http://www.cinechronicle.com/wp-content/uploads/2011/07/Cardigan-Dude-The-Big-Lebowski.jpg",
+        scores: ["4", "3", "1", "2", "2", "1", "3", "2", "5", "2"]
+    },
+    {
+        name: "Blume",
+        photo: "http://www.indiewire.com/wp-content/uploads/2016/05/tumblr-m5sqf33ebp1qhrqkso1-1280.jpg",
+        scores: ["5", "4", "4", "5", "2", "3", "1", "4", "5", "3"]
+    },
+    {
+        name: "Bluto",
+        photo: "https://az616578.vo.msecnd.net/files/2016/01/02/635873689157228995952164191_Freshman%20Year.jpg",
+        scores: ["1", "3", "2", "3", "5", "1", "2", "2", "3", "3"]
+    },
 ];
 
 // export the friends data
